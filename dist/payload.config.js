6766f36bd47f885e73bd5a56735a3edbeafd10bb
Aimberê Rocha Mendonça <aimbererm@gmail.com>
2024-01-13 14:49:17 -0300

    feat: deploy northflank

--- dist/payload.config.js
+++ dist/payload.config.js
@@ -11,7 +11,7 @@ var path_1 = __importDefault(require("path"));
 var media_1 = require("./collections/media");
 var bundler_vite_1 = require("@payloadcms/bundler-vite");
 exports.default = (0, config_1.buildConfig)({
-    serverURL: process.env.NEXT_PUBLIC_SERVER_URL,
+    serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
     collections: [
         projects_1.Projects,
         media_1.Media
